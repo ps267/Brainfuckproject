471e9318e224f0053843f8935562f78b22d7b96c
Peter Schöppl <peterschoppl@Peters-MacBook-Pro.local>
2018-04-04 11:17:00 +0200

    best change ever

--- Brainfuckproject/main.cpp
+++ Brainfuckproject/main.cpp
@@ -8,8 +8,64 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+struct ToDo{
+    int nr;
+    char what[1024];
+    
+};
+using namespace std;
+int main(int argc, const char * argv[])
+{
+    int i=0;
+    ToDo* t;
+    ToDo** tpp=new ToDo*[32];
+    
+    for(int j=0;j<32;j++)
+    {
+        tpp[j]=new ToDo;
+    }
+    
+    for(;i<=31;i++)
+    {
+        cout<<"Nummer"<<endl;
+        cin>>tpp[i]->nr;
+        if(tpp[i]->nr==0)
+            break;
+        
+    }
+    
+    for(bool x=true;x==true;)
+    {
+        x=false;
+        for(int lauf=0;lauf<i-1;lauf++)
+        {
+            if(tpp[lauf]->nr>tpp[lauf+1]->nr)
+            {
+                t=tpp[lauf];
+                tpp[lauf]=tpp[lauf+1];
+                tpp[lauf+1]=t;
+                x=true;
+                
+            }
+        }
+    }
+    
+    for(int lauf=0;lauf<i;lauf++)
+    {
+        cout<< tpp[lauf]->nr;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
