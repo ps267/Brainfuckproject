e05bfdb52f44d86d7d8649b1488caa0beff0b602
Peter Schöppl <peterschoppl@peters-macbook-pro.local>
2018-04-04 10:12:19 +0200

    Initial Commit

---
+++ Brainfuckproject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Brainfuckproject
+//
+//  Created by Peter Schöppl on 04.04.18.
+//  Copyright © 2018 Peter Schöppl. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
